0d3758b52485023dc4186fd87bd3bd8c8fd5e0ed
Petr Kozelka <pkozelka@gmail.com>
2022-10-12 04:28:25 +0200

    array of C strings

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
+use std::borrow::Cow;
 use std::ffi::{CStr, CString};
 use std::os::raw::c_char;
 use std::path::PathBuf;
+use std::ptr;
 use dlopen2::wrapper::Container;
 use dlopen2::wrapper::WrapperApi;
 
@@ -14,9 +16,13 @@ struct RawApi {
     #[dlopen2_name = "MOJO_NewModel"]
     new_model: unsafe extern "C" fn(filename: *const c_char, tf_lib_prefix: *const c_char) -> *const MojoPipeline,
     #[dlopen2_name = "MOJO_FeatureNum"]
-    feature_num: unsafe extern "C" fn(pipeline: *const MojoPipeline) -> u8,
+    feature_num: unsafe extern "C" fn(pipeline: *const MojoPipeline) -> isize,
+    #[dlopen2_name = "MOJO_FeatureNames"]
+    feature_names: unsafe extern "C" fn(pipeline: *const MojoPipeline) -> *const *const c_char,
     #[dlopen2_name = "MOJO_OutputNum"]
-    output_num: unsafe extern "C" fn(pipeline: *const MojoPipeline) -> u8,
+    output_num: unsafe extern "C" fn(pipeline: *const MojoPipeline) -> isize,
+    #[dlopen2_name = "MOJO_OutputNames"]
+    output_names: unsafe extern "C" fn(pipeline: *const MojoPipeline) -> *const *const c_char,
     #[dlopen2_name = "MOJO_DeleteModel"]
     delete_model: unsafe extern "C" fn(pipeline: *const MojoPipeline),
 }
@@ -24,6 +30,7 @@ struct RawApi {
 fn main() {
     // using bindgen: https://medium.com/dwelo-r-d/using-c-libraries-in-rust-13961948c72a
     // https://github.com/shepmaster/rust-ffi-omnibus + http://jakegoulding.com/rust-ffi-omnibus/
+    // char ** : http://adam.younglogic.com/2019/03/accessing-c-arrays-of-string-from-rust/
 
 
     // println!("DAIMOJO Version: {}", mojo_version().to_string_lossy());
@@ -55,6 +62,38 @@ fn main() {
     let ocnt = unsafe { cont.output_num(pipeline) };
     println!("output num: {ocnt}");
 
+    unsafe {
+        let mpa = cont.feature_names(pipeline);
+        if mpa != ptr::null() {
+            for i in 0..icnt {
+                let p: *const c_char = *(mpa.offset(i));
+                let name = CStr::from_ptr(p);
+                println!("{i}. {}", name.to_string_lossy());
+            }
+        }
+    };
+
+    let outputs = charpp_to_strs(unsafe { cont.output_names(pipeline) }, ocnt);
+    println!("Outputs:");
+    for name in outputs {
+        println!("* {name}");
+    }
+
     unsafe { cont.delete_model(pipeline) }
     println!("deleted");
+}
+
+
+fn charpp_to_strs<'a>(charpp: *const *const c_char, count: isize) -> Vec<Cow<'a,str>> {
+    let mut vec = Vec::new();
+    unsafe {
+        if charpp != ptr::null() {
+            for i in 0..count {
+                let p: *const c_char = *(charpp.offset(i as isize));
+                let name = CStr::from_ptr(p);
+                vec.push(name.to_string_lossy());
+            }
+        }
+    }
+    vec
 }
