de57075378d8dd55ce4a6488c53084f8e7e6b6ba
Petr Kozelka <pkozelka@gmail.com>
2022-10-13 00:55:58 +0200

    moved notes to markdown

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,5 @@ extern crate core;
 
 
 fn main() {
-    // using bindgen: https://medium.com/dwelo-r-d/using-c-libraries-in-rust-13961948c72a
-    // https://github.com/shepmaster/rust-ffi-omnibus + http://jakegoulding.com/rust-ffi-omnibus/
-    // char ** : http://adam.younglogic.com/2019/03/accessing-c-arrays-of-string-from-rust/
-    //      https://doc.rust-lang.org/std/slice/fn.from_raw_parts_mut.html
-    //      https://stackoverflow.com/questions/48657360/change-c-array-element-via-rust-ffi
-
 
 }
